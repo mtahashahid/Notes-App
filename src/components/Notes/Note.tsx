11de722b1fab4e626f6ff9f7c9443b1e641436a7
Taha <taha_shahid@outlook.com>
2021-08-26 15:51:14 +0500

    some fixes

--- src/components/Notes/Note.tsx
+++ src/components/Notes/Note.tsx
@@ -1,14 +1,25 @@
-import { FC } from "react"; //it for defining type
+import { FC,FocusEvent } from "react"; //it for defining type
 import INote from "../../interfaces/notes.interface";
 import "./Note.css";
 type Props = {
   note: INote;
 };
+const noteTextUpdated = (event: FocusEvent<HTMLDivElement>) => {
+  console.log(event.currentTarget.textContent)
+};
 const Note: FC<Props> = ({ note }) => {
   return (
     <div className="note">
-      <div contentEditable className="note__text">{note.text}</div>
+      <div
+        contentEditable
+        suppressContentEditableWarning={true}
+        className="note__text"
+        onBlur={noteTextUpdated}
+      >
+        {note.text} 
+      </div>
       <div className="note__link">
+        {/* <a href={note.link}>Link</a> */}
         <a href={note.link}>Link</a>
       </div>
     </div>
